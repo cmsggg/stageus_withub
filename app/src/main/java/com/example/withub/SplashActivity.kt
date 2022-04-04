435953d597a243a7d5b741af8eefcd5189276437
SJMoons <ferrari_919@naver.com>
2022-04-04 20:26:23 +0900

    Make login page

--- app/src/main/java/com/example/withub/SplashActivity.kt
+++ app/src/main/java/com/example/withub/SplashActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
 
         Log.d("message","abddsf")
     }
